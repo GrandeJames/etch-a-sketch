383fed04091f57955f32c6dd06f60c3a391cf2d0
James Grande <james.grande6@gmail.com>
2021-07-12 08:38:08 -1000

    Add color options

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ function start() {
     const DEFAULT_SIZE = 10;
 
     addCellsToSketchpad(DEFAULT_SIZE);
-    addCellHoverListener();
+    addCellHover(() => getRainbowColor());
     changeSize(DEFAULT_SIZE);
     clear();
+
+    onColorButtonClick();
 }
 
 function addCellsToSketchpad(size) {
@@ -16,22 +18,12 @@ function addCellsToSketchpad(size) {
     sketchpad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < (size * size); i++) {
-        
+
         const cell = document.createElement("div");
         cell.className = "cell"
 
         sketchpad.appendChild(cell);
     }
-}
-
-function addCellHoverListener() {
-    const cells = document.querySelectorAll(".cell");
-
-    cells.forEach(cell => {
-        cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
-        })
-    });
 }
 
 function removeCells() {
@@ -56,7 +48,7 @@ function changeSize(defaultSize) {
         }
         removeCells();
         addCellsToSketchpad(size);
-        addCellHoverListener();
+        addCellHover( () => getRainbowColor());
     })
 }
 
@@ -67,12 +59,62 @@ function resetCells() {
 }
 
 function clear() {
-    console.log("test");
     const button = document.querySelector("#clear")
-
     button.addEventListener("click", () => resetCells());
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
+}
+
+function addCellHover(color) {
+    const cells = document.querySelectorAll(".cell");
+
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", () => {
+
+            // Needs to be a function because we want rainbow color 
+            cell.style.backgroundColor = color();
+        })
+    });
+}
+
+function getRainbowColor() {
+    return `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
+}
+
+function onColorButtonClick() {
+    const colorButtons = document.querySelectorAll(".color-button");
+
+    colorButtons.forEach(colorButton => {
+        
+        colorButton.addEventListener("click", () => {
+
+            if (!(colorButton.classList.contains("activated"))) {
+                turnOffActivatedButton();
+                colorButton.classList.toggle("activated");
+
+            }
+
+            // TODO change to whatever color clicked
+            if (colorButton.id === "black") {
+                addCellHover(() => "black");
+            } else if (colorButton.id === "rainbow") {
+                addCellHover(() => getRainbowColor());
+            } else if (colorButton.id === "eraser") {
+                addCellHover(() => "#fff");
+            }
+        });
+    });
+}
+
+function turnOffActivatedButton() {
+    const colorButtons = document.querySelectorAll(".color-button");
+
+    colorButtons.forEach(colorButton => {
+        if (colorButton.classList.contains("activated")) {
+            colorButton.classList.toggle("activated");
+
+        }
+    });
 }
